1db53477bd8892a3a435561358ddd41733881c7a
Vlad Oseyev <vladoseyev3@gmail.com>
2018-01-12 14:50:07 -0500

    Added to roots

--- index.php
+++ index.php
@@ -4,8 +4,23 @@ require_once('vendor/autoload.php');
 
 $f3 = Base::instance();
 
+//Set debug level
+$f3->set('DEBUG', 3);
+
 $f3->route('GET /', function() {
     echo '<h1>Routing Demo</h1>';
+}
+);
+
+//Define a page1 route
+$f3->route('GET /page1', function() {
+    echo '<h1>This is page 1</h1>';
+}
+);
+
+//Define a page1 subpage-a route
+$f3->route('GET /page1/subpage-a', function() {
+    echo '<h1>This is page 1, subpage a</h1>';
 }
 );
 
