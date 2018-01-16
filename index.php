5521091bdbd1960126d72ba404b409f6a971bd6e
Vlad Oseyev <vladoseyev3@gmail.com>
2018-01-16 14:25:49 -0500

    Added to roots

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -31,6 +33,29 @@ $f3->route('GET /hello/@name', function($f3, $params) {
     $f3->set('name', $params['name']);
     $template = new Template();
     echo $template->render('views/hello.html');
+}
+);
+
+//Define a parameter into template
+$f3->route('GET /hi/@first/@last', function($f3, $params) {
+
+    $f3->set('first', $params['first']);
+    $f3->set('last', $params['last']);
+    $f3->set('message', 'Hi');
+
+    $_SESSION['first'] = $f3->get('first');
+    $_SESSION['last'] = $f3->get('last');
+
+
+    $template = new Template();
+    echo $template->render('views/hi.html');
+}
+);
+
+//Define a parameter into template
+$f3->route('GET /hi-again', function($f3, $params) {
+    echo 'Hi again, ' . $_SESSION['first'];
+
 }
 );
 
