a3f6e6040b340791e58592f7949325b444a51e46
Vlad Oseyev <vladoseyev3@gmail.com>
2018-01-16 13:44:30 -0500

    Added to roots

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once('vendor/autoload.php');
 
 $f3 = Base::instance();
@@ -9,6 +13,36 @@ $f3->set('DEBUG', 3);
 
 $f3->route('GET /', function() {
     echo '<h1>Routing Demo</h1>';
+}
+);
+
+$f3->route('GET /hello/@name', function($f3, $params) {
+    $name = $params['name'];
+    echo "<h1>Hello, $name</h1>";
+}
+);
+
+$f3->route('GET /language/@lang', function($f3, $params) {
+    switch($params['lang']){
+        case 'swahili';
+            echo 'Jumbo!'; break;
+
+        case 'spanish';
+            echo 'Hola!'; break;
+
+        case 'russian';
+            echo 'Privet!'; break;
+
+        case 'farsi';
+            echo 'Salam!'; break;
+
+            //Reroute to another page
+        case 'french';
+            $f3->reroute('/');
+        default:
+            echo 'Hello!';
+    }
+
 }
 );
 
@@ -23,6 +57,15 @@ $f3->route('GET /page1/subpage-a', function() {
     echo '<h1>This is page 1, subpage a</h1>';
 }
 );
+
+//Define a Template route
+$f3->route('GET /jewlery/rings/toe-rings', function() {
+    //echo '<h1>Buy a toe ring today!</h1>';
+    $template = new Template();
+    echo $template->render('views/toe-rings.html');
+}
+);
+
 
 $f3->run();
 
