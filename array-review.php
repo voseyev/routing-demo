ebb05907ab663f7938b63cab00f4265a0a911f00
Vlad Oseyev <vladoseyev3@gmail.com>
2018-01-17 04:18:30 -0500

    Commiting in routing-demo

---
+++ array-review.php
@@ -0,0 +1,66 @@
+<?php
+
+global $animals;
+
+$animals = array("alpaca", "boa", "panda");
+$animalsAdd = array("alpaca");
+
+printAnimals($animals);
+
+for($x = 0; $x < 2; $x++) {
+
+    if (in_array("goat", $animalsAdd)) {
+        animals($animalsAdd[$x]);
+        printAnimals($animals);
+    }
+
+    else {
+        echo "Adding" . " " . $animalsAdd[$x] . "...";
+        array_push($animalsAdd, "goat");
+        echo nl2br(" \n ");
+        printAnimals($animals, $animalsAdd);
+        array_push($animals, "goat");
+        }
+
+
+}
+function printAnimals($animals, $animalsAdd){
+    echo $animals[0] . " " . $animals[1] . " " . $animals[2] . " " . $animals[3];
+    echo  nl2br (" \n ");
+}
+
+function animals($string) {
+    echo "Adding" . " " . $string . "...";
+    array_push($animals, "goat");
+    echo  nl2br ("\n");
+}
+
+$flavors = array("grasshopper" => "The GrassHopper", "maple" => "Whiskey Maple Bacon", "carrot" => "Carrot Walnut",
+    "caramel" => "Salted Caramel Cupcake" , "velvet" => "Red Velvet", "lemon" => "Lemon Drop", "tiramisu" => "Tiramisu");
+
+echo  nl2br ("\n");
+echo  nl2br ("\n");
+
+foreach ($flavors as $key => $value) {
+    echo $key . " - ";
+    echo $value;
+    echo  nl2br ("\n");
+}
+
+echo  nl2br ("\n");
+
+?>
+
+<?php
+$flavors = array("grasshopper" => "The GrassHopper", "maple" => "Whiskey Maple Bacon", "carrot" => "Carrot Walnut",
+"caramel" => "Salted Caramel Cupcake" , "velvet" => "Red Velvet", "lemon" => "Lemon Drop", "tiramisu" => "Tiramisu");
+
+    foreach ($flavors as $key => $value) { ?>
+        <html>
+            <body>
+                <input type="checkbox" name="flavors" value=<? $key ?>> <? echo $value ?><br>
+            </body>
+        </html>
+<?php } ?>
+
+
